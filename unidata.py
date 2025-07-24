bf52838400b1d676f20eebe51093f38bf2a5613d
John Boyce <johnboyce@Johns-MacBook-Air.local>
2025-07-24 08:36:37 +0100

    Second major commit to project. Includes updated Template HTML for base, homepage and reports. App script has been updated to test connection to first two tables provided by DB team.

--- unidata.py
+++ unidata.py
@@ -8,7 +8,7 @@ DB_CONFIG = {
     'user': 'test_account',
     'password': 'bluemacbook',
     'host': 'localhost',
-    'database': 'mydb'
+    'database': 'endofmod'
 }
 
 # Template definitions
@@ -81,7 +81,7 @@ def search():
             query += " AND Last_Name LIKE %s"
             params.append(f"%{surname}%")
 
-    ## carries out the SQL Query ##
+    ## connects to the database and carries out the SQL Query ##
 
     conn = get_connection()
     cursor = conn.cursor()
@@ -98,14 +98,80 @@ def search():
                            col_names=col_names,
                            request=request)
 
-## route to the pre-compiled reports page ##
+## route to the pre-compiled reports initial page ##
 @app.route('/reports')
 def reports():
     return render_template(REPORTS_TEMPLATE, context=any)
 
-@app.route('/viewreport')
+### route to the VIEW REPORT page, which displays the report chosen on the Reports page
+
+@app.route('/view_report')
 def viewreport():
-    return render_template(VIEWREPORT_TEMPLATE, context=any)
+    selection = request.args.get('selection')
+    id = request.args.get('id')
+    surname = request.args.get('surname')
+
+    if not selection:
+        return render_template('view_report.html', selection=None, results=None, col_names=None, request=request)
+
+## start assembling the SQL query
+
+    query = []
+    params = []
+    print(selection)
+
+### logic to choose correct SQL dependent on entity being queried ## 
+
+    if selection == 'Students without Courses':
+            query = "SELECT * FROM Departments"
+            
+    elif selection == 'report_2':
+        if id:
+            query += " AND student_id = %s"
+            params.append(id)
+        if surname:
+            query += " AND Last_Name LIKE %s"
+            params.append(f"%{surname}%")
+
+    elif selection == 'report_3':
+        if id:
+            query += " AND id = %s"
+            params.append(id)
+        if surname:
+            query += " AND Last_Name LIKE %s"
+            params.append(f"%{surname}%")
+
+    elif selection == 'report_4':
+        if id:
+            query += " AND id = %s"
+            params.append(id)
+        if surname:
+            query += " AND Last_Name LIKE %s"
+            params.append(f"%{surname}%")
+
+    elif selection == 'report_5':
+        if id:
+            query += " AND id = %s"
+            params.append(id)
+        if surname:
+            query += " AND Last_Name LIKE %s"
+            params.append(f"%{surname}%")
+
+    ## carries out the SQL Query ##
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(query, params)
+    results = cursor.fetchall()
+    col_names = [desc[0] for desc in cursor.description]
+    cursor.close()
+    conn.close()
+
+## returns the search template with complete search results ## 
+    return render_template(VIEWREPORT_TEMPLATE,
+                           selection=selection,
+                           results=results,
+                           col_names=col_names,
+                           request=request)
 
 if __name__ == '__main__':
     app.run(debug=True)
