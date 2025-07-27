04a51e11c707e56143750a8d18948b166dc8304a
John Boyce <johnboyce@Johns-MacBook-Air.local>
2025-07-27 22:08:42 +0100

    Updated Home/welcome page. Added new Student Search and Student View functionality.

--- unidata.py
+++ unidata.py
@@ -17,19 +17,23 @@ HOME_TEMPLATE = 'home_page.html'
 SEARCH_TEMPLATE = 'search.html'
 REPORTS_TEMPLATE = 'reports.html'
 VIEWREPORT_TEMPLATE = 'view_report.html'
-SEARCH_RESULTS_TEMPLATE = 'search_results.html'
+VIEWSTUDENT_TEMPLATE = 'view_student.html'
+STUDENT_SEARCH_TEMPLATE = 'student_search.html'
 
 
 ## DB helper
 def get_connection():
     return mysql.connector.connect(**DB_CONFIG)
 
+    """
+    FLASK ROUTES TO HOME AND SEARCH PAGES WITHIN THE APP
+    """
 ## route to homepage
 @app.route('/')
 def home():
     return render_template(HOME_TEMPLATE, context=any)
 
-## route to combined search and results page ##
+## route to combined STAFF search and results page ##
 
 @app.route('/search')
 def search():
@@ -53,7 +57,10 @@ def search():
     table = entity_table_map[entity]
     query = f"SELECT * FROM {table} WHERE 1=1"
     params = []
-    print(entity)
+
+    """
+    SEARCH LOGIC TO APPEND CORRECT SQL DEPENDING ON TABLE BEING QUERIED    
+    """
 
 ### logic to append correct SQL dependent on entity being queried ## 
 
@@ -98,6 +105,10 @@ def search():
                            col_names=col_names,
                            request=request)
 
+    """
+    Section deals with nagigation to the Reports and View Reports pages, and logic to assemble SQL Request for View Reports
+    """
+
 ## route to the pre-compiled reports initial page ##
 @app.route('/reports')
 def reports():
@@ -125,7 +136,7 @@ def viewreport():
     if selection == 'High Performing Students':
         query = """
         
-        select rc.student_id, s.first_name, s.last_name, AVG(grade) > 70 from registered_courses rc 
+        select rc.student_id, s.first_name, s.last_name, AVG(grade), AVG(grade) > 70 from registered_courses rc
         inner join students s on s.student_id = rc.student_id
         group by rc.student_id
         having AVG(rc.grade) > 70;
@@ -150,9 +161,7 @@ def viewreport():
         inner join students s on s.student_id = rc.student_id
         where 1=1
         """
-        print(argument)
         if not argument == ['']:
-            print("I think there's an argument")
             query += " AND (l.last_name = %s OR l.lecturer_id = %s)"
             params = (argument[0], argument[0])
             
@@ -192,6 +201,97 @@ def viewreport():
                            results=results,
                            col_names=col_names,
                            request=request)
+
+## route to combined STUDENT search and results page ##
+
+@app.route('/student_search')
+def student_search():
+    student_id = request.args.get('student_ID')
+    surname = request.args.get('surname')
+
+    query = """
+    select student_ID, first_name, last_name from students
+    where 1+1
+    """
+    params = []
+
+    """
+    SEARCH LOGIC TO APPEND CORRECT SQL DEPENDING ON TABLE BEING QUERIED    
+    """
+
+### logic to append correct SQL dependent on field being queried ## 
+
+    if student_id:
+        query += " AND student_id = %s"
+        params.append(student_id)
+
+    if surname:
+        query += " AND last_name LIKE %s"
+        params.append(surname)
+
+    elif not student_id and not surname:
+        return render_template(STUDENT_SEARCH_TEMPLATE,
+                               results=None)
+            
+    ## connects to the database and carries out the SQL Query ##
+
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(query, params)
+    results = cursor.fetchall()
+    col_names = [desc[0] for desc in cursor.description]
+    cursor.close()
+    conn.close()
+
+
+## returns the search template with complete search results ## 
+    return render_template(STUDENT_SEARCH_TEMPLATE,
+                           surname=surname,
+                           student_id=student_id,
+                           results=results,
+                           col_names=col_names,
+                           request=request)
+
+"""
+SECTION FOR VIEW STUDENT PAGE
+"""
+@app.route('/view_student')
+def viewstudent():
+    student_id = request.args.get('student_id')
+
+## start assembling the SQL query
+
+    query = []
+    params = []
+
+### logic to map create SQL Query for additional Student Data ##
+
+    query = """
+            select s.student_id, s.first_name, s.last_name, s.program_enroll_in, CONCAT(l.first_name, ' ', l.last_name) as Lecturer from students s
+            inner join Lecturers l on s.advisor = l.Lecturer_id
+            where 1=1
+            """
+    
+    query += " AND s.student_id = %s"
+    params.append(student_id)
+    
+    print(f"THE QUERY IS: {query} ")
+    ## carries out the SQL Query ##
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(query, params)
+    results = cursor.fetchall()
+    col_names = [desc[0] for desc in cursor.description]
+    cursor.close()
+    conn.close()
+
+## returns the view student template with complete details ## 
+    return render_template(VIEWSTUDENT_TEMPLATE,
+                           student_id=student_id,
+                           results=results,
+                           col_names=col_names,
+                           request=request)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
